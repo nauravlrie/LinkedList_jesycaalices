1ad05fd6ac13aabd4c5f75ee9098bfe6b8e3adcd
nauravlrie <nauravlrie@gmail.com>
2026-05-06 11:08:14 +0700

    2.2 Modifikasi Elemen pada Single Linked List2

--- pertemuan12/src/SLLMain11.java
+++ pertemuan12/src/SLLMain11.java
@@ -2,7 +2,7 @@ public class SLLMain11 {
     public static void main(String[] args) {
         SingleLinkedList11 sll = new SingleLinkedList11();
 
-        // Data Mahasiswa dari Jobsheet
+        
         Mahasiswa11 mhs1 = new Mahasiswa11("24212200", "Alvaro", "1A", 4.00);
         Mahasiswa11 mhs2 = new Mahasiswa11("23212201", "Bimon", "2B", 3.80);
         Mahasiswa11 mhs3 = new Mahasiswa11("22212202", "Cintia", "3C", 3.50);
